a1ddda0c7884cab0a103e301a685a2d4b7c2b4d0
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-09 01:25:50 +0600

    Nvim: Modify LuaSnip config

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -27,72 +27,43 @@ return {
             -- local m = require("luasnip.extras").match
             -- local n = require("luasnip.extras").nonempty
             -- local dl = require("luasnip.extras").dynamic_lambda
-            -- local fmt = require("luasnip.extras.fmt").fmt
+            local fmt = require("luasnip.extras.fmt").fmt
             -- local fmta = require("luasnip.extras.fmt").fmta
             -- local types = require("luasnip.util.types")
             -- local conds = require("luasnip.extras.conditions")
             -- local conds_expand = require("luasnip.extras.conditions.expand")
 
-            ls.add_snippets(nil, {
-                fish = {
-                    snip({ trig = "fun", namr = "Function", dscr = "Function" },
-                    {
-                        text({"function "}), insert(1, "func_name"), text({"", "\t"}), insert(2), text({"", "end", "", ""}), insert(0)
-                    }),
+            ls.add_snippets("fish", {
+                snip("fun", fmt("function {}\n\t{}\nend\n\n{}", {insert(1, "func_name"), insert(2), insert(0)})),
 
-                    snip({ trig = "switch", namr = "Switch", dscr = "Switch statement" },
-                    {
-                        text({"switch $"}), insert(1, "arg"), text({"", "    case '"}), insert(2, "case"), text({"'", "        "}), insert(3), text({"", "end", "", ""})
-                    }),
+                snip("switch", fmt("switch ${}\n\tcase '{}'{}\nend\n\n{}", {insert(1, "arg"), insert(2, "case"), insert(3), insert(0)})),
 
-                    --TODO: Add condition to trigger only inside switch statement
-                    snip({ trig = "case", namr = "Switch Case", dscr = "Switch Case" },
-                    {
-                        text({"case '"}), insert(1, "case"), text({"'", "    "}), insert(2)
-                    }),
+                snip("case", fmt("case '{}'{}", {insert(1, "case"), insert(0)})),
 
-                    snip({ trig = "if", namr = "If", dscr = "If Statement" },
-                    {
-                        text({"if "}), insert(1, "condition"), text({"", "    "}), insert(2), text({"", "end", "", ""})
-                    }),
+                snip("if", fmt("if {}\n\t{}\nend\n\n{}", {insert(1, "condition"), insert(2), insert(0)})),
 
-                    snip({ trig = "elif", namr = "If/Else-If", dscr = "If/Else-If Statement" },
-                    {
-                        text({"if "}), insert(1, "condition"), text({"", "    "}), insert(2), text({"", "else if "}), insert(3, "condition"), text({"", "    "}), insert(4), text({"", "else "}), insert(5, "condition"), text({"", "    "}), insert(6), text({"", "end", "", ""})
-                    }),
-
-                    snip({ trig = "else", namr = "If/Else", dscr = "If/Else Statement" },
-                    {
-                        text({"if "}), insert(1, "condition"), text({"", "    "}), insert(2), text({"", "else "}), insert(3, "condition"), text({"", "    "}), insert(4), text({"", "end", "", ""})
-                    }),
-                },
-
-                --TODO: Add condition to trigger at start of line
-                markdown = {
-                    snip({ trig = "csnote", namr = "CS Note", dscr = "Metadata for CS Notes", }, {
-                        text({"---", "Type: Permanent Note", "Topic: CS", "Title: "}), insert(1), text({"", "Related: "}), insert(2), text({"", "---", "", "", ""}), insert(0)
-                    }),
-
-                    snip({ trig = "langnote", namr = "Language Note", dscr = "Metadata for Language Notes", }, {
-                        text({"---", "Type: Permanent Note", "Topic: Language/"}), insert(1, "English"), text({"", "Title: "}), insert(2), text({"", "---", "", "", ""}), insert(0)
-                    }),
-
-                    snip({ trig = "litnote", namr = "Literature Note", dscr = "Metadata for Literature Notes", }, {
-                        text({"---", "Type: Literature Note", ""}), insert(1, "Book"), text({": "}), insert(2), text({"", "Author: "}), insert(3), text({"", "---", "", "", ""}),
-                        insert(0)
-                    }),
-
-                    snip({ trig = "wikinote", namr = "Wiki Note", dscr = "Metadata for Wiki notes", }, {
-                        text({"---", "Type: Permanent Note", "Topic: "}), insert(1), text({"", "Topic"}), insert(2), text({"", "Related: "}), insert(3), text({"", "---", "", "", ""}), insert(0)
-                    }),
-
-                    snip({ trig = "src", namr = "Source", dscr = "Sources cited from", }, {
-                        text({"Source", "---", ""}), insert(1), }),
-
-                    snip({ trig = "rel", namr = "Related", dscr = "Related files", }, {
-                        text({"[[./"}), insert(1), text({"]] "}), insert(0) }),
-                }
             })
+
+            ls.add_snippets("markdown", {
+                snip("permanote", fmt("---\nType: Permanent Note\nTopic: {}\nTitle: {}\nRelated: {}\n---\n\n{}", {insert(1, "CS"), insert(2, "Title"), insert(3), insert(0) } )),
+
+                snip("litnote", fmt("---\nType: Literature Note\n{}\n---\n\n{}", {choice(1, {
+                    fmt("Book: {}\nAuthor: {}", {insert(1, "Title"), insert(2, "Author")}),
+                    fmt("Article: [{}]({})\n", {insert(1, "Title"), insert(2, "URL")}),
+                    fmt("Podcast: [{}]({})\n", {insert(1, "Title"), insert(2, "URL")}),
+                    fmt("Video: [{}]({})\n", {insert(1, "Title"), insert(2, "URL")}), }), insert(0) } )),
+
+                snip("rel", fmt("[[./{}]]", {insert(1),})),
+                snip("src", fmt("Source\n---\n{}", {insert(1),})),
+            })
+
+            local map = vim.api.nvim_set_keymap
+            map("i", "<C-n>", "<Plug>luasnip-next-choice", {})
+            map("s", "<C-n>", "<Plug>luasnip-next-choice", {})
+            map("i", "<C-p>", "<Plug>luasnip-prev-choice", {})
+            map("s", "<C-p>", "<Plug>luasnip-prev-choice", {})
+
         end
     }
 }
+
