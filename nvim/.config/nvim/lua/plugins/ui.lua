029709d9b2e9b6f6365246bdabd1bd26e781d0b8
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-12 15:38:13 +0600

    Nvim: Replace colorscheme plugin with kanagawa.nvim

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,10 +1,42 @@
 return {
-    --Colorscheme
+    --Colorscheme: https://github.com/rebelot/kanagawa.nvim
     {
-        'Everblush/nvim',
-        name = 'everblush',
+        'rebelot/kanagawa.nvim',
+        name = 'kanagawa',
         config = function ()
-            vim.cmd.colorscheme("everblush")
+            require('kanagawa').setup{
+                dimInactive = true,
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none",
+                            }
+                        }
+                    }
+                },
+                overrides = function(colors)
+                    local theme = colors.theme
+                    return {
+                        NormalFloat = { bg = "none" },
+                        FloatBorder = { bg = "none" },
+                        FloatTitle = { bg = "none" },
+
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        -- set their background accordingly if you wish to keep them dark and borderless
+                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                    }
+                end,
+                theme = "dragon", --available: dragon, wave, lotus
+                background = {
+                    dark = "dragon",
+                    light = "lotus"
+                },
+            }
+            vim.cmd.colorscheme('kanagawa')
         end
-    },
+    }
 }
