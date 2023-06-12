0f08ab4cfe41ec2d8c241f3f6498a5dfc51cb026
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-12 18:23:30 +0600

    Nvim: Modify kanagawa.nvim config

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -8,35 +8,39 @@ return {
                 dimInactive = true,
                 colors = {
                     theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none",
-                            }
-                        }
+                        all = { ui = { bg_gutter = "none", } }
                     }
                 },
                 overrides = function(colors)
                     local theme = colors.theme
                     return {
+
+                        -- Transparent floating windows
                         NormalFloat = { bg = "none" },
                         FloatBorder = { bg = "none" },
                         FloatTitle = { bg = "none" },
-
                         NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-                        -- Popular plugins that open floats will link to NormalFloat by default;
-                        -- set their background accordingly if you wish to keep them dark and borderless
                         LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
                         MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+                        -- Borderless Telescope
+                        TelescopeTitle = { fg = theme.ui.special, bold = true },
+                        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+                        -- Darker completion menu
+                        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1, blend = vim.o.pumblend },
+                        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                        PmenuSbar = { bg = theme.ui.bg_m1 },
+                        PmenuThumb = { bg = theme.ui.bg_p2 },
                     }
                 end,
-                theme = "dragon", --available: dragon, wave, lotus
-                background = {
-                    dark = "dragon",
-                    light = "lotus"
-                },
             }
-            vim.cmd.colorscheme('kanagawa')
+            require('kanagawa').load('dragon')
         end
     }
 }
