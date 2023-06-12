b2a875eb2e82b2f07da8540dfa738326220dc7da
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-12 15:47:56 +0600

    Wezterm: Change colorscheme to kanagawa

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -15,7 +15,6 @@ local config = {
 
     -- VISUAL
 
-    color_scheme = "Everblush",
     inactive_pane_hsb = { hue = 1.0, saturation = 0.4, brightness = 0.9 },
     hide_tab_bar_if_only_one_tab = true,
     use_fancy_tab_bar = false,
@@ -23,6 +22,28 @@ local config = {
     window_decorations = "TITLE",
     font_size = 17,
     font = wezterm.font_with_fallback { 'DroidSansMono Nerd Font', 'FiraCode Nerd Font', },
+
+    -- COLOR
+
+    force_reverse_video_cursor = true,
+    colors = {
+        foreground = "#c5c9c5",
+        background = "#181616",
+
+        cursor_bg = "#c8c093",
+        cursor_fg = "#c8c093",
+        cursor_border = "#c8c093",
+
+        selection_fg = "#c8c093",
+        selection_bg = "#2d4f67",
+
+        scrollbar_thumb = "#16161d",
+        split = "#16161d",
+
+        ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+        brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+        indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+    },
 
     -- KEYMAP
 
