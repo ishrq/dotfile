8068381d394cecdfd83dda4d5695d54db6c8fac2
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-13 12:50:49 +0600

    Nvim: Install plugin hihglight-undo.nvim

--- nvim/.config/nvim/lua/plugins/general.lua
+++ nvim/.config/nvim/lua/plugins/general.lua
@@ -3,6 +3,13 @@ return{
     {
         "nvim-lua/plenary.nvim",
     },
+    -- https://github.com/tzachar/highlight-undo.nvim
+    {
+        'tzachar/highlight-undo.nvim',
+        config = function()
+            require('highlight-undo').setup()
+        end
+    },
     --Lists
     {
         "gaoDean/autolist.nvim",
