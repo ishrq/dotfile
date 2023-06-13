a3339cc5b43f1fbc4db9b4ea9d30b921cfc0001e
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-13 23:00:47 +0600

    Nvim: Modify snippets

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -24,7 +24,7 @@ return {
                     diagnostics.fish,
                     diagnostics.phpcs,
                     diagnostics.todo_comments,
-                    -- diagnostics.trail_space,
+                    diagnostics.trail_space,
                     diagnostics.write_good,
                     formatting.black,
                     formatting.codespell,
