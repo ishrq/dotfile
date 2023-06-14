835192cbb979fe927773245b7c85525ba11f8ddf
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-14 17:59:27 +0600

    Nvim: Modify config for markdown-preview.nvim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -26,8 +26,17 @@ return {
         lazy = true,
         build = function() vim.fn['mkdp#util#install']() end,
         config = function ()
+            vim.g.mkdp_auto_close = 0
+            vim.g.mkdp_command_for_global = 1
             vim.g.mkdp_open_to_the_world = 1
-            -- vim.g.mkdp_browser = '/usr/bin/librewolf --new-tab'
+            vim.g.mkdp_open_ip = ''
+            vim.g.mkdp_port = ''
+            vim.g.mkdp_markdown_css = ''
+            vim.g.mkdp_highlight_css = ''
+            vim.g.mkdp_theme = 'dark'
+            -- vim.g.mkdp_filetypes = 'markdown'
+            -- vim.g.mkdp_page_title = '「${name}」'
+            -- vim.g.mkdp_browser = 'librewolf --new-tab'
         end
     },
 }
