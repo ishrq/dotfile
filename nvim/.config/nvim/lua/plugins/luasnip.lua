a663615990d5a4e96f5827ab7cd004f8c7f459b6
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-15 13:50:17 +0600

    Nvim: Modify snippets

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -18,36 +18,6 @@ return {
         config = function()
             require('luasnip.loaders.from_vscode').lazy_load({ exclude = {'all', 'fish', 'html', 'markdown'} })
             require('luasnip.loaders.from_lua').lazy_load()
-
-            local ls = require('luasnip')
-            local snip = ls.snippet
-            -- local node = ls.snippet_node
-            -- local text = ls.text_node
-            local insert = ls.insert_node
-            local func = ls.function_node
-            local choice = ls.choice_node
-            -- local dynamic = ls.dynamic_node
-            -- local r = ls.restore_node
-            -- local l = require('luasnip.extras').lambda
-            -- local rep = require('luasnip.extras').rep
-            -- local p = require('luasnip.extras').partial
-            -- local m = require('luasnip.extras').match
-            -- local n = require('luasnip.extras').nonempty
-            -- local dl = require('luasnip.extras').dynamic_lambda
-            local fmt = require('luasnip.extras.fmt').fmt
-            -- local fmta = require('luasnip.extras.fmt').fmta
-            -- local types = require('luasnip.util.types')
-            -- local conds = require('luasnip.extras.conditions')
-            -- local conds_expand = require('luasnip.extras.conditions.expand')
-
-            ls.add_snippets('markdown', {
-                snip('litnote', fmt('---\nType: Literature Note\n{}\n---\n\n\n{}', {choice(1, {
-                    fmt('Book: {}\nAuthor: {}', {insert(1, 'Title'), insert(2, 'Author')}),
-                    fmt('Article: [{}]({})\n', {insert(1, 'Title'), insert(2, 'URL')}),
-                    fmt('Podcast: [{}]({})\n', {insert(1, 'Title'), insert(2, 'URL')}),
-                    fmt('Video: [{}]({})\n', {insert(1, 'Title'), insert(2, 'URL')}), }), insert(0) } )),
-            })
-
         end
     }
 }
