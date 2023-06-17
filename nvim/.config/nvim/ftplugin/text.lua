4c1dc3c9db7eea3eac32d8512b02ac4f590b97b9
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-17 22:31:08 +0600

    Nvim: Modify ftplugin files

--- nvim/.config/nvim/ftplugin/text.lua
+++ nvim/.config/nvim/ftplugin/text.lua
@@ -4,22 +4,15 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 
 
--- markdown-preview.nvim
-vim.keymap.set('n', '<C-p>', '<Plug>MarkdownPreviewToggle', {desc='Markdown Preview'})
-
-
--- MiniAi textobjects
+-- Mini.ai textobjects
 
 local spec_pair = require('mini.ai').gen_spec.pair
 vim.b.miniai_config = {
-  -- TODO: add textobject for footnotes & tasks
-  custom_textobjects = {
-    ['*'] = spec_pair('*', '*', { type = 'greedy' }),
-    ['_'] = spec_pair('_', '_', { type = 'greedy' }),
-    ['~'] = spec_pair('~', '~', { type = 'greedy' }),
-    d = {{ '()%d%d%d%d[-|/]%d%d[-|/]%d%d()', '()%d%d[-|/]%d%d[-|/]%d%d%d%d()', }}, -- date
-    u = {{ 'https://[%www.][%S]+', 'http://[%www.][%S]+' }}, -- url
-  },
+    -- TODO: add textobject for footnote
+    custom_textobjects = {
+        d = {{ '()%d%d%d%d[-|/]%d%d[-|/]%d%d()', '()%d%d[-|/]%d%d[-|/]%d%d%d%d()', }}, -- date
+        u = {{ 'https://[%www.][%S]+', 'http://[%www.][%S]+' }}, -- url
+    },
 }
 
 vim.keymap.set({'n','x','o'}, ']d', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'd', {n_lines='200'})<CR>", {desc='Next date'})
