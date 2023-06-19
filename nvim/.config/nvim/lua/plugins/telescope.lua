58b1bfba8b09957f73cf0202428059b3d25c7669
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-19 20:43:09 +0600

    Nvim: Cleanup code

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -26,74 +26,22 @@ return {
     config = function()
       require('telescope').load_extension('fzf')
       require('telescope').load_extension('undo')
-      vim.keymap.set('n', '<leader>u', '<cmd>Telescope undo<cr>', {desc = 'Telescope Undo'})
+      vim.keymap.set('n', '<leader>u', '<cmd>Telescope undo<cr>', {desc='Telescope Undo'})
     end,
     keys = {
-      {
-        '<leader>fd',
-        function() require('telescope.builtin').find_files() end,
-        desc = 'Find file',
-      },
-      {
-        '<leader>rg',
-        function() require('telescope.builtin').live_grep() end,
-        desc = 'Grep',
-      },
-      {
-        '<leader>gf',
-        function() require('telescope.builtin').git_files() end,
-        desc = 'Git files',
-      },
-      {
-        '<leader>gc',
-        function() require('telescope.builtin').git_commits() end,
-        desc = 'Git commits',
-      },
-      {
-        '<leader>gs',
-        function() require('telescope.builtin').git_status() end,
-        desc = 'Git status',
-      },
-      {
-        '<leader>b',
-        function() require('telescope.builtin').buffers() end,
-        desc = 'Buffers',
-      },
-      {
-        '<leader>j',
-        function() require('telescope.builtin').jumplist() end,
-        desc = 'Jumplist',
-      },
-      {
-        '<leader>k',
-        function() require('telescope.builtin').keymaps() end,
-        desc = 'Keymaps',
-      },
-      {
-        '<leader>m',
-        function() require('telescope.builtin').marks() end,
-        desc = 'Marks',
-      },
-      {
-        '<leader>Q',
-        function() require('telescope.builtin').diagnostics() end,
-        desc = 'Diagnostics',
-      },
-      {
-        '<leader>y',
-        function() require('telescope.builtin').registers() end,
-        desc = 'Registers',
-      },
-      {
-        '<leader>1',
-        function() require('telescope.builtin').oldfiles() end,
-        desc = 'Recent files',
-      },
-      {
-        'z=',
-        function() require('telescope.builtin').spell_suggest() end,
-        desc = 'Spell suggestions',
-      },
+      { '<leader>fd', function() require('telescope.builtin').find_files() end, desc='Find file' },
+      { '<leader>rg', function() require('telescope.builtin').live_grep() end, desc='Grep' },
+      { '<leader>gf', function() require('telescope.builtin').git_files() end, desc='Git files' },
+      { '<leader>gc', function() require('telescope.builtin').git_commits() end, desc='Git commits' },
+      { '<leader>gs', function() require('telescope.builtin').git_status() end, desc='Git status' },
+      { '<leader>b', function() require('telescope.builtin').buffers() end, desc='Buffers' },
+      { '<leader>j', function() require('telescope.builtin').jumplist() end, desc='Jumplist' },
+      { '<leader>k', function() require('telescope.builtin').keymaps() end, desc='Keymaps' },
+      { '<leader>m', function() require('telescope.builtin').marks() end, desc='Marks' },
+      { '<leader>Q', function() require('telescope.builtin').diagnostics() end, desc='Diagnostics' },
+      { '<leader>y', function() require('telescope.builtin').registers() end, desc='Registers' },
+      { '<leader>1', function() require('telescope.builtin').oldfiles() end, desc='Recent files' },
+      { 'z=', function() require('telescope.builtin').spell_suggest() end, desc='Spell Suggestions ' },
     },
   },
 }
