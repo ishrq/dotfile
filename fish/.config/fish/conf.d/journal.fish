49c68566a24eb0b2c448a28fce7b19463e90dae1
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-20 12:50:34 +0600

    Fish: Improve custom scripts
    Test for directory instead of $USER to set $file
    Provide feedback (echo) only if entry added successfully
    Remove old files

--- fish/.config/fish/conf.d/journal.fish
+++ fish/.config/fish/conf.d/journal.fish
@@ -7,22 +7,23 @@
 
 function journal
   set -f day $(date +"%F")
-  if test $USER = "u0_a1692" || test $USER = "u0_a215" #Termux S8,M10
-    set -f file ~/storage/shared/Documents/COMPUTER/ARCHIVE/Journal/journal.txt
+
+  if test -d "$HOME/ARCHIVE"
+    set file ~/ARCHIVE/Journal/journal.txt
   else
-    set -f file ~/ARCHIVE/Journal/journal.txt
+    set file ~/storage/shared/Documents/COMPUTER/ARCHIVE/Journal/journal.txt
   end
+
   switch $argv
     case ''
       $EDITOR $file
     case $argv
       set -f entry "$argv"
       if grep -q "^$day" $file
-        sed -i "s/$day/$day\\n$entry/g" $file
+        sed -i "s/$day/$day\\n$entry/g" $file && echo "[Entry added on $day]"
       else
-        sed -i "s/===/===\\n\\n$day\\n$entry/g" $file
+        sed -i "s/===/===\\n\\n$day\\n$entry/g" $file && echo "[Entry created for $day]"
       end
-        echo "[Entry added on $day]"
   end
 end
 
