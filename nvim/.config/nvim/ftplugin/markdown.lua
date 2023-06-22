6e1ce77ae15700a346f604d2d8fef9ca78dc67cc
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-23 00:10:56 +0600

    Nvim: Code cleanup

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,17 +1,15 @@
 -- Indent 2 spaces
-vim.o.tabstop = 2
+vim.o.tabstop     = 2
 vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.shiftwidth  = 2
 
 -- character column
-vim.o.cc          = 81
+vim.o.cc = 81
 
 -- markdown-preview.nvim
 vim.keymap.set('n', '<C-p>', '<Plug>MarkdownPreviewToggle', {desc='Markdown Preview'})
 
-
 -- MiniAi textobjects
-
 local spec_pair = require('mini.ai').gen_spec.pair
 vim.b.miniai_config = {
   -- TODO: add textobject for footnotes & tasks
@@ -24,10 +22,11 @@ vim.b.miniai_config = {
   },
 }
 
-vim.keymap.set({'n','x','o'}, ']d', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'd', {n_lines='200'})<CR>", {desc='Next date'})
-vim.keymap.set({'n','x','o'}, '[d', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'd', {search_method='prev', n_lines='200'})<CR>", {desc='Previous date'})
-vim.keymap.set({'n','x','o'}, ']u', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'u', {n_lines='200'})<CR>", {desc='Next url'})
-vim.keymap.set({'n','x','o'}, '[u', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'u', {search_method='prev', n_lines='200'})<CR>", {desc='Previous url'})
+set local nxo = {'n', 'x', 'o'}
+vim.keymap.set(nxo, ']d', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'd', {n_lines='200'})<CR>", {desc='Next date'})
+vim.keymap.set(nxo, '[d', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'd', {search_method='prev', n_lines='200'})<CR>", {desc='Previous date'})
+vim.keymap.set(nxo, ']u', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'u', {n_lines='200'})<CR>", {desc='Next url'})
+vim.keymap.set(nxo, '[u', "<Cmd>lua MiniAi.move_cursor('left', 'i', 'u', {search_method='prev', n_lines='200'})<CR>", {desc='Previous url'})
 
 
 -- MiniBracketed
