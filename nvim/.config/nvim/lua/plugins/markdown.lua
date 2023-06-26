cfd02beb7bddd35f1be0ea578d49769ed482b068
ishrq <123639873+ishrq@users.noreply.github.com>
2023-06-26 19:57:09 +0600

    Nvim: Remove plugin autolist.nvim

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,23 +1,4 @@
 return {
-  -- https://github.com/gaoDean/autolist.nvim
-  {
-    'gaoDean/autolist.nvim',
-    ft = {'markdown', 'text', 'tex', 'plaintex'},
-    event = 'InsertEnter',
-    config = function()
-      local autolist = require('autolist')
-      autolist.setup()
-      autolist.create_mapping_hook('i', '<CR>', autolist.new)
-      -- autolist.create_mapping_hook('i', '<Tab>', autolist.indent)
-      -- autolist.create_mapping_hook('i', '<S-Tab>', autolist.indent, '<C-D>')
-      autolist.create_mapping_hook('n', 'o', autolist.new)
-      autolist.create_mapping_hook('n', 'O', autolist.new_before)
-      -- autolist.create_mapping_hook('n', '>>', autolist.indent)
-      -- autolist.create_mapping_hook('n', '<<', autolist.indent)
-      autolist.create_mapping_hook('n', '<C-r>', autolist.force_recalculate)
-      autolist.create_mapping_hook('n', '<leader>x', autolist.invert_entry, "")
-    end,
-  },
   -- https://github.com/iamcco/markdown-preview.nvim
   {
     'iamcco/markdown-preview.nvim',
