17e92d91cc6b4d331014afffc5ae29b5a4c15347
ishrq <123639873+ishrq@users.noreply.github.com>
2023-08-15 19:17:34 +0600

    Nvim: Add keymap to toggle !important in css

--- nvim/.config/nvim/ftplugin/css.lua
+++ nvim/.config/nvim/ftplugin/css.lua
@@ -2,3 +2,15 @@
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+
+-- Toggle !important
+-- https://nanotipsforvim.prose.sh/toggle-important-in-css
+vim.keymap.set("n", "<leader>i", function ()
+  local lineContent = vim.fn.getline('.')
+  if lineContent:find("!important") then
+    lineContent = lineContent:gsub(" !important", "")
+  else
+    lineContent = lineContent:gsub(";", " !important;")
+  end
+  vim.fn.setline(".", lineContent)
+end)
