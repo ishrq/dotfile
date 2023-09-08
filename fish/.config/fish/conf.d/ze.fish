be1fb4c56dc54a3ce88ab0220cf89c498a2da31c
ishrq <123639873+ishrq@users.noreply.github.com>
2023-09-08 19:03:11 +0600

    Fish: Rename ze.fish to zz.fish

--- fish/.config/fish/conf.d/ze.fish
+++
@@ -1,6 +0,0 @@
-# Author: IA
-# ---
-
-function zl -w z -d 'change directory and list file'
-  z $argv && lsd
-end
---
+++ fish/.config/fish/conf.d/zz.fish
@@ -0,0 +1,6 @@
+# Author: IA
+# ---
+
+function zz -w z -d 'change directory and list file'
+  z $argv && lsd
+end
