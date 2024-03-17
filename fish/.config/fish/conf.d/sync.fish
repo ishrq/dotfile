bacbafaabf19beaa9ec6d710584e6be51342b6f8
ishrq <123639873+ishrq@users.noreply.github.com>
2024-03-17 15:01:25 +0600

    Refactor

--- fish/.config/fish/conf.d/sync.fish
+++ fish/.config/fish/conf.d/sync.fish
@@ -11,7 +11,7 @@ function sync -d "Sync files between devices"
   end
 
   # phone
-  if test $USER = "u0_a1692" || test $USER = "u0_a215" || test $USER = "u0_a241"
+  if test $USER = "u0_a1692" || test $USER = "u0_a215"
     if test $USER = "u0_a1692" #S8
       set -g device "s8"
     else if test $USER = "u0_a215" #M10
@@ -38,9 +38,9 @@ function ____phone-to-phone
       set_color red; echo "$target is offline"
     else
       curl -S "$target_ip:1821/sshd"
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/shared/Documents/COMPUTER/ $target:~/storage/shared/Documents/COMPUTER/
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
-      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
+      for dir in ~/storage/shared/Documents/COMPUTER/ ~/.config/newsboat/ ~/.local/share/newsboat/
+        rsync -amuqz --log-file=$log --mkpath --partial $dir $target:$dir
+      end
       set_color green; echo "Sync with $target successful"
     end
   end
@@ -51,16 +51,16 @@ function ____phone-to-laptop
   if ping -c 1 192.168.0.104 | grep -q "Unreachable"
     set_color red; echo "Laptop is offline"
   else
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/shared/Documents/COMPUTER/ laptop:~/
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/dcim/Screenshots/ laptop:~/ARCHIVE/Screenshots/
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/storage/shared/backups/ laptop:~/ARCHIVE/Phone-Backups/Backup-$device/
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ laptop:~/.local/share/newsboat/
-    rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ laptop:~/.config/newsboat/
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/fish/config.fish laptop:~/.config/fish/
-    rsync -ahmuqz --log-file=$log --mkpath --partial --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ laptop:~/.config/fish/conf.d/
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/snippets/ laptop:~/.config/nvim/snippets/
-    rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/skeletons/ laptop:~/.config/nvim/skeletons/
-    rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config.local ~/.config/git/config laptop:~/.config/git/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/storage/shared/Documents/COMPUTER/ laptop:~/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/storage/dcim/Screenshots/ laptop:~/ARCHIVE/Screenshots/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/storage/shared/backups/ laptop:~/ARCHIVE/Phone-Backups/Backup-$device/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ laptop:~/.local/share/newsboat/
+    rsync -amuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ laptop:~/.config/newsboat/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/.config/fish/config.fish laptop:~/.config/fish/
+    rsync -amuqz --log-file=$log --mkpath --partial --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ laptop:~/.config/fish/conf.d/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/.config/nvim/snippets/ laptop:~/.config/nvim/snippets/
+    rsync -amuqz --log-file=$log --mkpath --partial ~/.config/nvim/skeletons/ laptop:~/.config/nvim/skeletons/
+    rsync -amuqz --log-file=$log --mkpath --partial --exclude=config.local ~/.config/git/config laptop:~/.config/git/
     set_color green; echo "Sync with laptop successful"
   end
 end
@@ -76,17 +76,17 @@ function ____laptop-to-phone
       set_color red; echo "$target is offline"
     else
       curl -S "$target_ip:1821/sshd"
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/RESOURCES/{Bookmarks,Culinary,House,IDs,Lists,Notes,Syllabus} $target:~/storage/shared/Documents/COMPUTER/RESOURCES/
+      rsync -amuqz --log-file=$log --mkpath --partial ~/RESOURCES/{Bookmarks,Culinary,House,IDs,Lists,Notes,Syllabus} $target:~/storage/shared/Documents/COMPUTER/RESOURCES/
 
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/ARCHIVE/{Finances,Journals,Logs,Medical,Receipts,References,Resume} $target:~/storage/shared/Documents/COMPUTER/ARCHIVE/
+      rsync -amuqz --log-file=$log --mkpath --partial ~/ARCHIVE/{Finances,Journals,Logs,Medical,Receipts,References,Resume} $target:~/storage/shared/Documents/COMPUTER/ARCHIVE/
 
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
-      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/fish/config.fish $target:~/.config/fish/
-      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ $target:~/.config/fish/conf.d/
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/snippets/ $target:~/.config/nvim/snippets/
-      rsync -ahmuqz --log-file=$log --mkpath --partial ~/.config/nvim/skeletons/ $target:~/.config/nvim/skeletons/
-      rsync -ahmuqz --log-file=$log --mkpath --partial --exclude=config.local ~/.config/git/config $target:~/.config/git/
+      rsync -amuqz --log-file=$log --mkpath --partial ~/.local/share/newsboat/ $target:~/.local/share/newsboat/
+      rsync -amuqz --log-file=$log --mkpath --partial --exclude=config ~/.config/newsboat/ $target:~/.config/newsboat/
+      rsync -amuqz --log-file=$log --mkpath --partial ~/.config/fish/config.fish $target:~/.config/fish/
+      rsync -amuqz --log-file=$log --mkpath --partial --exclude={autopair,fzf,sponge}.fish ~/.config/fish/conf.d/ $target:~/.config/fish/conf.d/
+      rsync -amuqz --log-file=$log --mkpath --partial ~/.config/nvim/snippets/ $target:~/.config/nvim/snippets/
+      rsync -amuqz --log-file=$log --mkpath --partial ~/.config/nvim/skeletons/ $target:~/.config/nvim/skeletons/
+      rsync -amuqz --log-file=$log --mkpath --partial --exclude=config.local ~/.config/git/config $target:~/.config/git/
       set_color green; echo "Sync with $target successful"
     end
   end
